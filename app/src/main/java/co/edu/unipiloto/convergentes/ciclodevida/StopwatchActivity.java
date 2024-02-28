efa54c423a16a665142fd1af244fa19a9c9fa764
ValentinaRibon23 <valentina-contreras@upc.edu.co>
2024-02-28 09:21:20 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/convergentes/ciclodevida/StopwatchActivity.java
@@ -0,0 +1,60 @@
+package co.edu.unipiloto.convergentes.ciclodevida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler; // Importar Handler
+import android.view.View;
+import android.widget.TextView; // Importar TextView
+import java.util.Locale; // Importar Locale
+
+public class StopwatchActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        runTimer(); // Llamar al método runTimer para iniciar el cronómetro
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+
+        final Handler handler = new Handler();
+
+        // Postear un Runnable en el Handler
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+
+                // Establecer el texto en el TextView
+                timeView.setText(time);
+
+                if (running) {
+                    seconds++;
+                }
+                // Postear el Runnable nuevamente después de 1000 ms (1 segundo)
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
